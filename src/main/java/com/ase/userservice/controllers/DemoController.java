95a08475668a81e4db3dd233233bb8f8a7a05f99
erik <erschmia@gmail.com>
2025-09-11 14:31:49 +0200

    try to implement route protection
    we just copy-pasted the code from the tutorial and hope that it works

--- src/main/java/com/ase/userservice/controllers/DemoController.java
+++ src/main/java/com/ase/userservice/controllers/DemoController.java
@@ -1,5 +1,6 @@
 package com.ase.userservice.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
