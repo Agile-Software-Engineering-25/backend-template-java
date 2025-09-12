d795b0b5388ad6db7ccddd89b6b55aa1a8c64fef
David Clara Figueiredo <david.clara.figueiredo@gmail.com>
2025-09-12 14:54:33 +0200

    add user model to extract info from jwt, change claim from which to get roles, need to add jwt mapping to model

--- src/main/java/com/ase/userservice/security/JwtAuthConverter.java
+++ src/main/java/com/ase/userservice/security/JwtAuthConverter.java
@@ -10,13 +10,18 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.lang.NonNull;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
  
 public class JwtAuthConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     
     @Override
-    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {
-        var roles = jwt.getClaimAsStringList("groups");
+    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {;
+        var claims = jwt.getClaimAsMap("realm_access");
+        if (claims == null || claims.isEmpty()) {
+            return List.of();
+        }
+        var roles = (List<String>) claims.get("groups");
         for (String role : roles) {
             System.out.println("Role from JWT: " + role);
         }
