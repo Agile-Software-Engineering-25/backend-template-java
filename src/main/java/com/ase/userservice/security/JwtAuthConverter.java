0f1834df1998c3213003f33571a0888e4446638b
erik <erschmia@gmail.com>
2025-09-22 13:29:19 +0200

    fix style and format
    the formatter uses some kind of formatting i cant properly understand on my machine using the redhat java formatter.

--- src/main/java/com/ase/userservice/security/JwtAuthConverter.java
+++ src/main/java/com/ase/userservice/security/JwtAuthConverter.java
@@ -1,4 +1,4 @@
-// based on this tutorial xdd: https://www.javacodegeeks.com/2025/07/spring-boot-keycloak-role-based-authorization.html
+// based on this tutorial: https://www.javacodegeeks.com/2025/07/spring-boot-keycloak-role-based-authorization.html
 
 package com.ase.userservice.security;
 
@@ -6,25 +6,21 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
-
 import org.springframework.lang.NonNull;
-
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
- 
+
 public class JwtAuthConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-    
+
     @Override
-    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {;
-        var claims = jwt.getClaimAsMap("realm_access");
-        if (claims == null || claims.isEmpty()) {
-            return List.of();
-        }
-        var roles = (List<String>) claims.get("groups");
-        for (String role : roles) {
-            System.out.println("Role from JWT: " + role);
-        }
+    public Collection<GrantedAuthority> convert(@NonNull Jwt jwt) {
+        var roles = jwt.getClaimAsStringList("groups");
+
+        // you can check the roles here if you want to
+        // for (String role : roles) {
+        // System.out.println("Role from JWT: " + role);
+        // }
         return roles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toUpperCase()))
                 .collect(Collectors.toList());
